e604974c314b7c8531519d62ed436ffbf5503acf
Yasunori M, Fukushima <developer.fukushima@gmail.com>
2019-12-26 08:24:49 +0800

    add abstract BasicStatistics

--- src/Data/Series.php
+++ src/Data/Series.php
@@ -2,27 +2,16 @@
 
 namespace devfym\IntelliPHP\Data;
 
-class Series
+use devfym\IntelliPHP\Math\BasicStatistic;
+
+class Series extends BasicStatistic
 {
-    private $data;
+    /**
+     * Series constructor.
+     */
 
     public function __construct()
     {
-        //
-    }
-
-    public function setList($l = []) : void
-    {
-        $this->data = $l;
-    }
-
-    public function getList() : array
-    {
-        return $this->data;
-    }
-
-    public function mean() : float
-    {
-        return round(array_sum($this->data) / count($this->data), 2);
+        parent::__construct();
     }
 }
--- tests/Data/DataFrameTest.php
+++ tests/Data/DataFrameTest.php
@@ -12,25 +12,39 @@ class DataFrameTest extends TestCase
         $df = new DataFrame();
 
         $data = [
-          'name' => ['a', 'b', 'c', 'd'],
-          'age'  => [12, 14, 16, 18],
+            'name' => ['a', 'b', 'c', 'd'],
+            'age'  => [12, 14, 16, 18],
+            'height' => [168, 172, 178, 180]
         ];
 
-        //データをセット
+        //DataFrameにデータを追加
         $df->readArray($data);
 
         //カラム名だけ取得
-        $this->assertEquals(['name', 'age'], $df->getColumns());
+        $this->assertEquals(['name', 'age','height'], $df->getColumns());
 
         //データのindexを取得
         $this->assertEquals(4, $df->getIndex());
 
         //Nameのseriesを取得
-        $this->assertEquals(['a', 'b', 'c', 'd'], $df->name->getList());
+        $this->assertEquals(['a', 'b', 'c', 'd'], $df->name->getSample());
 
         //Ageの平均値を取得
         $this->assertEquals(15, $df->age->mean());
 
-        //print_r($df->getObjectVariables());
+        //Ageの最大値を取得
+        $this->assertEquals(18, $df->age->max());
+
+        //Ageの最小値を取得
+        $this->assertEquals(12, $df->age->min());
+
+        //DataFrameにあるすべての平均値を取得
+        $this->assertEquals(['age' => 15, 'height' => 174.5], $df->mean());
+
+        //DataFrameにあるすべての最大値を取得
+        $this->assertEquals(['age' => 18, 'height' => 180], $df->max());
+
+        //DataFrameにあるすべての最小値を取得
+        $this->assertEquals(['age' => 12, 'height' => 168], $df->min());
     }
 }
